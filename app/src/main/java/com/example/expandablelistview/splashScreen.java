2e33c5ae3d1df98db833a858e3f0e3141a4261cd
asif331001 <asif95331001@gmail.com>
2023-02-23 16:30:47 +0600

    add splash screen

---
+++ app/src/main/java/com/example/expandablelistview/splashScreen.java
@@ -0,0 +1,50 @@
+package com.example.expandablelistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class splashScreen extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private int progress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBarId);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+            doWork();
+            startApp();
+            }
+        });
+        thread.start();
+    }
+
+    public void  doWork(){
+
+        for (progress=20; progress<=100; progress = progress+20){
+
+            try {
+                Thread.sleep(1000);
+                progressBar.setProgress(progress);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+    public void startApp(){
+
+        Intent intent = new Intent(splashScreen.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
